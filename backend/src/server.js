510c0813a667f5c7fbb97434cf2dc260eb64f039
Kaushal Raj Sonkar <kaushalrajsonkar@gmail.com>
2026-03-04 22:33:06 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
+const coursesRoutes = require("./routes/courses");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/courses", coursesRoutes);
 
 app.get("/api/health",(req,res)=>{
   res.json({status:"ok"});
